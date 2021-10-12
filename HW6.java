b36d26c1ffed160324014081599aa4db86e7c357
andrei_20121 <pinzari-andrei@mail.ru>
2021-10-12 17:07:34 +0300

    new1

---
+++ HW6.java
@@ -0,0 +1,48 @@
+package Homework;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+
+public class HW6 {
+
+    private static final String HOST = "dataservice.accuweather.com";
+    private static final String FORECAST = "forecasts";
+    private static final String VERSION = "v1";
+    private static final String TYPE = "daily";
+    private static final String PERIOD = "5day";
+
+    private static final String SPB_KEY = "295212";
+    private static final String API_KEY = "6WLGmbmDhALN61PBUdqddw4rwk64Ar3k";
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(HOST)
+                .addPathSegment(FORECAST)
+                .addPathSegment(VERSION)
+                .addPathSegment(TYPE)
+                .addPathSegment(PERIOD)
+                .addPathSegment(SPB_KEY)
+                .addQueryParameter("apikey", API_KEY)
+                .addQueryParameter("language", "ru-ru")
+                .build();
+
+        System.out.println(url.toString());
+
+        Request requesthttp = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build();
+
+        String jsonResponse = client.newCall(requesthttp).execute().body().string();
+        System.out.println(jsonResponse);
+    }
+}
+
+
